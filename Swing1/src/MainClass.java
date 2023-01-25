89ea1de8cf78efd375139c652801329c75c84e13
Chiara Soulié <Chiara Souli@10.187.7.0>
2023-01-25 11:55:51 +0100

    show configurated window

--- Swing1/src/MainClass.java
+++ Swing1/src/MainClass.java
@@ -1,9 +1,12 @@
-
+import javax.swing.*;
 public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Coucou");
+		JFrame myWindow = new JFrame();
+		myWindow.setSize(200, 200);
+		myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		myWindow.setVisible(true);
 
 	}
 
