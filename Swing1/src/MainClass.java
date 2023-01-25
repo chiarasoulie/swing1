336372b4a8e062efcc30d9be124f1d6c8f280580
Chiara Soulié <Chiara Souli@10.187.7.0>
2023-01-25 12:28:40 +0100

    final cursus

--- Swing1/src/MainClass.java
+++ Swing1/src/MainClass.java
@@ -4,6 +4,20 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		JFrame myWindow = new JFrame();
+		JButton jb = new JButton("Click me!");
+		JButton jb2 = new JButton("Coucou");
+		JButton jb3 = new JButton("C'est un bouton");
+		
+		JPanel panel = new JPanel();
+		
+		panel.add(jb2);
+		panel.add(jb);
+		panel.add(jb3);
+		
+		myWindow.add(panel);
+		jb.addActionListener((e) -> { 
+			System.out.println("Coucou");
+		});
 		myWindow.setSize(200, 200);
 		myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		myWindow.setVisible(true);
